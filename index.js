5fd1c1c2bb9b7396a2dbc2d779c610fb5561bb0c
mix irving <whimful@gmail.com>
2018-03-20 22:37:30 +1300

    catch non-objects!

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = function getMsgContent (obj) {
+  if (typeof obj !== 'object') return
+
   if (obj.key && obj.value && obj.value.content) return obj.value.content
   else if (obj.author && obj.timestamp && obj.content) return obj.content
   else return obj
